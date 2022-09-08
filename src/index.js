ee5a0c47d3be2e9c940e050db6d4230cccebf144
ayrton <ayrton.parkinson1@gmail.com>
2022-09-08 19:03:54 -0400

    Import open weather module

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('hi');
+import {
+  getCurrentWeather,
+  getFiveDayForecast,
+  parseWeatherData,
+  parseForecastData,
+} from './openWeather';
+
+getCurrentWeather('Ottawa').then((data) => console.log(data));
+getCurrentWeather('Ottawa').then((data) => console.log(parseWeatherData(data)));
+getFiveDayForecast('Ottawa').then((data) => console.log(data));
+getFiveDayForecast('Ottawa').then((data) => console.log(parseForecastData(data)));
