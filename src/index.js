6b6f760271b8ec13f5c36285f99d9b8859251fae
ayrton <ayrton.parkinson1@gmail.com>
2022-09-09 12:46:37 -0400

    Import functions to populate weather divs

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import {
+  setBackgroundImage,
+  populateCurrentWeather,
+  populate24HourForecast,
+} from './pageLoad';
+import {
   getCurrentWeather,
   getFiveDayForecast,
   parseWeatherData,
   parseForecastData,
 } from './openWeather';
 
-getCurrentWeather('Ottawa').then((data) => console.log(data));
-getCurrentWeather('Ottawa').then((data) => console.log(parseWeatherData(data)));
-getFiveDayForecast('Ottawa').then((data) => console.log(data));
-getFiveDayForecast('Ottawa').then((data) => console.log(parseForecastData(data)));
+// getCurrentWeather('Ottawa').then((data) => console.log(data));
+// getCurrentWeather('Ottawa').then((data) => console.log(parseWeatherData(data)));
+// getFiveDayForecast('Ottawa').then((data) => console.log(data));
+// getFiveDayForecast('Ottawa').then((data) => console.log(parseForecastData(data)));
+
+getCurrentWeather('Ottawa').then((data) => parseWeatherData(data))
+  .then((obj) => {
+    setBackgroundImage(obj);
+    populateCurrentWeather(obj);
+  });
+
+getFiveDayForecast('Ottawa').then((data) => parseForecastData(data))
+  .then((arr) => {
+    populate24HourForecast(arr.slice(0, 10));
+  });
