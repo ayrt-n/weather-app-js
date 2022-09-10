de48e2d004140d4766372c89a96773afa03c234f
ayrton <ayrton.parkinson1@gmail.com>
2022-09-10 08:51:39 -0400

    Add form for user to search and fetch weather for certain location

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import {
   setBackgroundImage,
   populateCurrentWeather,
   populate24HourForecast,
+  populate5DayForecast,
+  populateLocationHeader,
 } from './pageLoad';
 import {
   getCurrentWeather,
@@ -15,13 +17,44 @@ import {
 // getFiveDayForecast('Ottawa').then((data) => console.log(data));
 // getFiveDayForecast('Ottawa').then((data) => console.log(parseForecastData(data)));
 
+const searchBtn = document.getElementById('search-button');
+const searchInput = document.getElementById('search');
+
+searchBtn.addEventListener('click', () => {
+  const searchValue = searchInput.value;
+
+  getCurrentWeather(searchValue).then((data) => parseWeatherData(data))
+    .then((obj) => {
+      setBackgroundImage(obj);
+      populateCurrentWeather(obj);
+      populateLocationHeader(obj.location);
+    });
+
+  getFiveDayForecast('Ottawa').then((data) => parseForecastData(data))
+    .then((arr) => {
+      const forecast24Hour = arr.slice(0, 10);
+      const forecast5Day = [];
+      for (let i = 7; i < arr.length; i += 8) { forecast5Day.push(arr[i]); }
+
+      populate24HourForecast(forecast24Hour);
+      populate5DayForecast(forecast5Day);
+    });
+});
+
+// By default load weather for 
 getCurrentWeather('Ottawa').then((data) => parseWeatherData(data))
   .then((obj) => {
     setBackgroundImage(obj);
     populateCurrentWeather(obj);
+    populateLocationHeader(obj.location);
   });
 
 getFiveDayForecast('Ottawa').then((data) => parseForecastData(data))
   .then((arr) => {
-    populate24HourForecast(arr.slice(0, 10));
+    const forecast24Hour = arr.slice(0, 10);
+    const forecast5Day = [];
+    for (let i = 7; i < arr.length; i += 8) { forecast5Day.push(arr[i]); }
+
+    populate24HourForecast(forecast24Hour);
+    populate5DayForecast(forecast5Day);
   });
