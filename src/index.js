dd6699c4609c4bcbb3a696929b05f684531a81cb
ayrton <ayrton.parkinson1@gmail.com>
2022-09-10 09:17:55 -0400

    Add catch statements for error handling to API calls

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import displayLoadingError from './displayErrors';
 import {
   setBackgroundImage,
   populateCurrentWeather,
@@ -28,17 +29,19 @@ searchBtn.addEventListener('click', () => {
       setBackgroundImage(obj);
       populateCurrentWeather(obj);
       populateLocationHeader(obj.location);
-    });
+    })
+    .catch(() => { displayLoadingError('current'); });
 
-  getFiveDayForecast('Ottawa').then((data) => parseForecastData(data))
+  getFiveDayForecast(searchValue).then((data) => parseForecastData(data))
     .then((arr) => {
-      const forecast24Hour = arr.slice(0, 10);
+      const forecast24Hour = arr.slice(0, 9);
       const forecast5Day = [];
       for (let i = 7; i < arr.length; i += 8) { forecast5Day.push(arr[i]); }
 
       populate24HourForecast(forecast24Hour);
       populate5DayForecast(forecast5Day);
-    });
+    })
+    .catch(() => { displayLoadingError('forecast'); });
 });
 
 // By default load weather for Ottawa
@@ -47,14 +50,16 @@ getCurrentWeather('Ottawa').then((data) => parseWeatherData(data))
     setBackgroundImage(obj);
     populateCurrentWeather(obj);
     populateLocationHeader(obj.location);
-  });
+  })
+  .catch(() => { displayLoadingError('forecast'); });
 
 getFiveDayForecast('Ottawa').then((data) => parseForecastData(data))
   .then((arr) => {
-    const forecast24Hour = arr.slice(0, 10);
+    const forecast24Hour = arr.slice(0, 9);
     const forecast5Day = [];
     for (let i = 7; i < arr.length; i += 8) { forecast5Day.push(arr[i]); }
 
     populate24HourForecast(forecast24Hour);
     populate5DayForecast(forecast5Day);
-  });
+  })
+  .catch(() => { displayLoadingError('forecast'); });
